9e36e2b0938a9280804dc32f5205a21da0c582f9
Bjorn Carlsson <bjorn.r.carlsson@schneider-electric.com>
2018-02-27 17:24:43 +0100

    added input component

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -1,32 +1,25 @@
 "use strict";
 
 var React = require('react');
+var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
     render: function() {
         return ( 
             <form>
                 <h1>Manage Author</h1>
-                <label htmlFor="firstName">First Name</label>
-                <input type="text"
+                <Input 
                     name="firstName"
-                    className="form-control"
-                    placeholder="First Name"
-                    ref="firstName"
+                    label="First Name"
+                    value={this.props.author.firstName}
                     onChange={this.props.onChange}
-                    value={this.props.author.firstName} />
-                <br />
-
-                <label htmlFor="lastName">Last Name</label>
-                <input type="text"
+                 />
+                <Input 
                     name="lastName"
-                    className="form-control"
-                    placeholder="Last Name"
-                    ref="lastName"
+                    label="Last Name"
+                    value={this.props.author.lastName}
                     onChange={this.props.onChange}
-                    value={this.props.author.lastName} />
-                <br />
-
+                 />
                 <input type="submit" value="Save" className="btn btn-default" />
             </form>
         );
