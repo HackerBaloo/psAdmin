3e30bb5dbc30caa6b81512891860da7e97d93714
Bjorn Carlsson <bjorn.r.carlsson@schneider-electric.com>
2018-02-27 10:47:37 +0100

    psadmin initial

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var React = require('react');
+var About = React.createClass( {
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>About</h1>
+        <p>This application uses the following tech:
+          <ul>
+            <li>React</li>
+            <li>React Router</li>
+            <li>Flux</li>
+            <li>Node</li>
+            <li>Gulp</li>
+            <li>Browserify</li>
+            <li>Bootstrap</li>
+          </ul>
+        </p>
+      </div>
+      );
+  }
+});
+
+module.exports = About;
