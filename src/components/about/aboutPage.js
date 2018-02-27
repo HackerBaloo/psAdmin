46ece3c75e4e58e8b227ba388c6b1b589aa87619
Bjorn Carlsson <bjorn.r.carlsson@schneider-electric.com>
2018-02-27 16:12:54 +0100

    willTransition checks added

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -2,7 +2,22 @@
 
 var React = require('react');
 var About = React.createClass( {
-  render: function() {
+  statics: {
+    willTransitionTo: function(transition, params, query, callback) {
+      if (!confirm('Are you sure you read a page tha\'s this boring')) {
+        transition.abort();
+      }
+      else {
+        callback();
+      }
+    },
+    willTransitionFrom: function(transition, component) {
+      if (!confirm('Are you sure you want to leave?')) {
+        transition.abort();
+      }
+    }
+  },
+render: function() {
     return (
       <div className="jumbotron">
         <h1>About</h1>
