2bb1f50a6db8ef5f843ac9cc9df3d696aff7a341
Bjorn Carlsson <bjorn.r.carlsson@schneider-electric.com>
2018-02-28 07:48:55 +0100

    save with toast

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,9 +1,15 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
+var Toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
+    mixins: [
+        Router.Navigation
+    ],
     getInitialState: function() {
         return {
             author: { id: '', firstName: '', lastName: '' }
@@ -17,11 +23,19 @@ var ManageAuthorPage = React.createClass({
         return this.setState({author: this.state.author});
     },
 
+    saveAuthor: function(event) {
+        event.preventDefault();
+        AuthorApi.saveAuthor(this.state.author);
+        Toastr.success('Author saved');
+        this.transitionTo('authors');
+    },
+
     render: function() {
         return ( 
             <div>
                 <AuthorForm author={this.state.author} 
-                onChange={this.setAuthorState} />
+                onChange={this.setAuthorState}
+                onSave={this.saveAuthor} />
             </div>
         );
     }
